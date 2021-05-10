504249dfb9641654785e0b2955c411b382f44310
sharifahmeeed <sharifahmeeed@gmail.com>
2021-05-10 10:47:12 +0600

    commit

--- main_program.py
+++ main_program.py
@@ -1,7 +1,13 @@
 import pyautogui
+import pyperclip as pp
 import time
 
 
 while(1):
-    time.sleep(3)
+    time.sleep(1)
     print(pyautogui.position(), pyautogui.size())
+    text ="Thursday January21."
+
+    pp.copy(text)
+    if(len(pp.paste())<80):
+        print(pp.paste())
